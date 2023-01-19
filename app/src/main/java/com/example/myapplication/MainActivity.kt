9b90ee28d080bb9642991ad5081879e92d9b0075
Lilnurs <zulpukarov518@icloud.com>
2023-01-19 14:22:47 +0600

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var login: TextView
+    private lateinit var textemail: EditText
+    private lateinit var textpusswod: EditText
+    private lateinit var button: Button
+    private lateinit var t: TextView
+    private lateinit var register: TextView
+
+
+
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        login = findViewById(R.id.login)
+        textemail = findViewById(R.id.email)
+        textpusswod = findViewById(R.id.password)
+        button = findViewById(R.id.button)
+        t = findViewById(R.id.t)
+        register = findViewById(R.id.register)
+        textemail.addTextChangedListener {
+            if (textemail.text.toString().isEmpty()){
+                button.setBackgroundColor(ContextCompat.getColor(this,R.drawable.bg_button)
+            }
+
     }
+}
 }
